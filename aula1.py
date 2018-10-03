7bf6a703067fee94fd35acb625a288ada764213b
marcellelopes <43679128+marcellelopes@users.noreply.github.com>
2018-10-03 14:24:18 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,62 @@
+print("exericio 1")
+
+dmilha = 10/1.61
+th = (43/60) + (30/3600)
+tseg = (43*60) + 30
+tmin = 43 + (30/60)
+
+vmh = dmilha/th
+vmmin = dmilha/tmin 
+vmseg = dmilha/tseg
+
+print(" ")
+
+print("a velocidade media e", vmh, "em milhas por hora")
+print("a velocidade media e", vmmin, "em milhas por minuto")
+print("a velocidade media e", vmseg, "em milhas por segundo")
+
+print(" ")
+print("exercicio 2")
+print(" ")
+vsom = 343
+t = 3
+
+d = vsom*t
+
+print("a distancia entre os fogos de artificio e a varanda e de", d, "em metros")
+print(" ")
+print("exercicio 3")
+print(" ")
+delta = (16 + (4*3*10))**(1/2)
+x1 = (4 + delta)/(2*3)
+x2 = (4 - delta)/(2*3)
+
+print("a delta da funcao e", delta)
+print("a primeira raiz e", x1)
+print("a segunda raiz e", x2)
+print(" ")
+print("exercicio 4")
+print(" ")
+sombra = 0.5
+altura = 5
+tg = 0.5/5
+
+print("o angulo zenital do sol e de", tg)
+print(" ")
+print("exercicio 5")
+print(" ")
+massabb = 11
+comprimento = 0.7
+imc = massabb/(comprimento*comprimento)
+
+print("o indice de massa corporal do bebe e de", imc, "em quilograma por metro ao quadrado")
+print(" ")
+print("exercicio 6")
+print(" ")
+gravidade = 9.8
+alturaobjeto = 3
+velocidadefinal = (2*gravidade*alturaobjeto)**(1/2)
+tempoqueda = velocidadefinal/gravidade
+
+print("a velocidade final de queda de um objeto e", velocidadefinal, "em metros por segundo")
+print(" o tempo de queda do objeto e", tempoqueda, "em segundos")
